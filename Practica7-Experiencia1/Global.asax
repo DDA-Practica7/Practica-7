be89650554ffd9acaa9469fb3c0ac7bc64980e1c
Jeremy Huaringa <jeremy.huaringa@ucsm.edu.pe>
2021-10-13 23:02:33 -0500

    Experiencia 1

---
+++ Practica7-Experiencia1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Practica7_Experiencia1.MvcApplication" Language="C#" %>
---
+++ Practica7-Experiencia1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Practica7_Experiencia1.Startup))]
+namespace Practica7_Experiencia1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
